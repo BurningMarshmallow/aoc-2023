c3ad07c08dd09add55e5a57d484e0426a66a27de
BurningMarshmallow <s.fedyanin@skbkontur.ru>
2023-12-01 10:45:28 +0700

    Change day 0 to solution of easy part of AoC 2022 Day 1

--- aoc/lib/day0.dart
+++ aoc/lib/day0.dart
@@ -1,5 +1,7 @@
+// Solution for easy part of https://adventofcode.com/2022/day/1
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 int calculate() {
   return 6 * 7;
@@ -13,7 +15,11 @@ Future<void> solveInternal() async {
   final lines = utf8.decoder
       .bind(File("input.txt").openRead())
       .transform(const LineSplitter());
-  await for (final line in lines) {
-    stdout.writeln(line);
-  }
+
+  var maxSum = await lines.fold(
+      (0, 0),
+      (prev, element) => element == ""
+          ? (max(prev.$1, prev.$2), 0)
+          : (prev.$1, prev.$2 + int.parse(element)));
+  print(maxSum);
 }
