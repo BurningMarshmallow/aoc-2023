59dbe92ad6732e1bfb47d38655243e628b9d208a
BurningMarshmallow <s.fedyanin@skbkontur.ru>
2023-12-02 14:13:30 +0700

    Remove real input from repo

--- aoc/bin/aoc.dart
+++ aoc/bin/aoc.dart
@@ -3,7 +3,7 @@ import 'package:aoc/day2.dart' as aoc;
 void main(List<String> arguments) {
   print("Sample");
   aoc.solve("sample.txt");
-  print("-------------------");
+  print("-------------------\n");
   print("Input");
   aoc.solve("input.txt");
 }
