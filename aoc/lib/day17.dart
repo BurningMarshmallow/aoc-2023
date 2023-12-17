5f1be149b8d12ec35d1ec67085ff24ef0ce6958c
BurningMarshmallow <s.fedyanin@skbkontur.ru>
2023-12-17 16:01:36 +0500

    Replace class to record with typedef

--- aoc/lib/day17.dart
+++ aoc/lib/day17.dart
@@ -19,7 +19,7 @@ void hard(List<List<int>> grid) {
 }
 
 int getMinHeatLoss(List<List<int>> grid, int minLength, int maxLength) {
-  var initState = State(0, 0, 0, 0, 0);
+  var initState = (0, 0, 0, 0, 0);
   var heap =
       HeapPriorityQueue<DistAndState>((x, y) => x.dist.compareTo(y.dist));
   heap.add(DistAndState(0, initState));
@@ -31,7 +31,14 @@ int getMinHeatLoss(List<List<int>> grid, int minLength, int maxLength) {
 
   while (true) {
     var curr = heap.removeFirst();
-    var state = curr.state;
+
+    var state = (
+      x: curr.state.$1,
+      y: curr.state.$2,
+      dx: curr.state.$3,
+      dy: curr.state.$4,
+      steps: curr.state.$5
+    );
     if (state.x == n - 1 && state.y == m - 1) {
       return curr.dist;
     }
@@ -43,7 +50,7 @@ int getMinHeatLoss(List<List<int>> grid, int minLength, int maxLength) {
         if (!inside(nx, ny, grid)) {
           continue;
         }
-        next.add(State(nx, ny, dx, dy, 1));
+        next.add((nx, ny, dx, dy, 1));
       }
     } else {
       if (state.steps < minLength) {
@@ -51,22 +58,22 @@ int getMinHeatLoss(List<List<int>> grid, int minLength, int maxLength) {
         if (!inside(nx, ny, grid)) {
           continue;
         }
-        next.add(State(nx, ny, state.dx, state.dy, state.steps + 1));
+        next.add((nx, ny, state.dx, state.dy, state.steps + 1));
       } else {
         for (var [nx, ny, dx, dy]
             in neighbours4WithDiff([state.x, state.y], n, m)) {
           if (!inside(nx, ny, grid)) {
             continue;
           }
-          if (isReverseDirection(dx, dy, state)) {
+          if (isReverseDirection(dx, dy, state.dx, state.dy)) {
             continue;
           }
-          if (isSameDirection(dx, dy, state)) {
+          if (isSameDirection(dx, dy, state.dx, state.dy)) {
             if (state.steps < maxLength) {
-              next.add(State(nx, ny, dx, dy, state.steps + 1));
+              next.add((nx, ny, dx, dy, state.steps + 1));
             }
           } else {
-            next.add(State(nx, ny, dx, dy, 1));
+            next.add((nx, ny, dx, dy, 1));
           }
         }
       }
@@ -77,71 +84,25 @@ int getMinHeatLoss(List<List<int>> grid, int minLength, int maxLength) {
         continue;
       }
       vis.add(state);
-      heap.add(DistAndState(curr.dist + grid[state.x][state.y], state));
+      var (nx, ny, _, _, _) = state;
+      heap.add(DistAndState(curr.dist + grid[nx][ny], state));
     }
   }
 }
 
-bool isSameDirection(int dx, int dy, State state) =>
-    dx == state.dx && dy == state.dy;
+bool isSameDirection(int dx, int dy, int stateDx, int stateDy) {
+  return dx == stateDx && dy == stateDy;
+}
 
-bool isReverseDirection(int dx, int dy, State state) {
-  return dx == -state.dx && dy == -state.dy;
+bool isReverseDirection(int dx, int dy, int stateDx, int stateDy) {
+  return dx == -stateDx && dy == -stateDy;
 }
 
 bool inside(int x, int y, List<List<int>> grid) {
   return 0 <= x && x < grid.length && 0 <= y && y < grid[0].length;
 }
 
-class State {
-  State(this.x, this.y, this.dx, this.dy, this.steps);
-  final int x, y, dx, dy, steps;
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) {
-      return true;
-    }
-    if (other.runtimeType != runtimeType) {
-      return false;
-    }
-    return other is State &&
-        other.x == x &&
-        other.y == y &&
-        other.dx == dx &&
-        other.dy == dy &&
-        other.steps == steps;
-  }
-
-  @override
-  int get hashCode => hash5(x, y, dx, dy, steps);
-
-  @override
-  String toString() {
-    return "$x $y $dx $dy $steps";
-  }
-}
-
-/// Generates a hash code for five objects.
-int hash5(a, b, c, d, e) => _finish(_combine(
-    _combine(
-        _combine(_combine(_combine(0, a.hashCode), b.hashCode), c.hashCode),
-        d.hashCode),
-    e.hashCode));
-
-// Jenkins hash functions
-
-int _combine(int hash, int value) {
-  hash = 0x1fffffff & (hash + value);
-  hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
-  return hash ^ (hash >> 6);
-}
-
-int _finish(int hash) {
-  hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
-  hash = hash ^ (hash >> 11);
-  return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
-}
+typedef State = (int x, int y, int dx, int dy, int steps);
 
 class DistAndState {
   DistAndState(this.dist, this.state);
